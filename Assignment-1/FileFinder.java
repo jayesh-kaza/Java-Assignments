dad7943c7d1fa04aef396a852a3b8b18eabfea4b
jayesh-kaza <52232556+jayesh-kaza@users.noreply.github.com>
2019-09-18 10:02:46 +0530

    Update and rename SearchDirectory.java to FileFinder.java

--- Assignment-1/FileFinder.java
+++ Assignment-1/FileFinder.java
@@ -3,23 +3,13 @@ import java.util.*;
 import java.util.regex.*; 
 
 /**
-     * Java program to search through the home directory and look for files that match a regular expression. 
-     * This program takes inputs repeatedly and prints out the full absolute path of the matching files found.
-     * 
      * Input : a regex
      * Output: absolute path of the files matching the regex
      * */
 
-public class SearchDirectory
+public class FileFinder
 {
-    
-    /** Method to find the files matching the given regex
-
-	<br>Parameters: String Regex, String Path
-	<br>Return Type: void
-	<br>Output : prints the absolute file path of the files with names matching the given pattern 
-   */
-    public static void finding(String reg,String ROOT_FILE_PATH) throws NullPointerException
+    public static void finding(String regex,String ROOT_FILE_PATH) throws NullPointerException
     {
         File f = new File(ROOT_FILE_PATH);
         if(!f.exists())
@@ -30,19 +20,17 @@ public class SearchDirectory
         {
             // Recursive call if it is a directory, to check the files inside it.
             if(file.isDirectory())
-                finding(reg,file.getAbsolutePath());
+                finding(regex,file.getAbsolutePath());
 
             else
             {
                 String fileName = file.getName();
-                if(Pattern.matches(reg,fileName))
+                if(Pattern.matches(regex,fileName))
                     System.out.println(file.getAbsolutePath());
             }
         }
     }
 	
-
-	/**This is the main method. It takes the regex as input */
     public static void main(String args[]) throws NullPointerException
     {
         String ROOT_FILE_PATH="/home";
@@ -51,8 +39,8 @@ public class SearchDirectory
         while(true)
         {
             System.out.print("Enter regex: ");
-            String reg = sc.nextLine();
-            finding(reg,ROOT_FILE_PATH);
+            String regex = sc.nextLine();
+            finding(regex,ROOT_FILE_PATH);
             
             System.out.print("Search again(Y/N):");
             String ch = sc.nextLine();
